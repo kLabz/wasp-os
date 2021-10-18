f4698d3a159201f17b93b2e6498c29fd6400ed95
k <k@klabz.org>
2021-10-18 15:52:41 +0200

    Fix main.py

--- wasp/main.py
+++ wasp/main.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 # Copyright (C) 2020 Daniel Thompson
 
-import watch
 from wasp import Wasp as wasp
-from gadgetbridge import *
-wasp.Wasp.start()
+# from gadgetbridge import *
+wasp.start()
