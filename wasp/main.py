8b806385c4446e40d612f4b486b6028510dd1084
k <k@klabz.org>
2021-10-18 12:17:45 +0200

    Tweak main.py

--- wasp/main.py
+++ wasp/main.py
@@ -1,8 +1,7 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 # Copyright (C) 2020 Daniel Thompson
 
+import watch
 from wasp import Wasp as wasp
-# TODO: fix that..
-# from gadgetbridge import *
-wasp.init()
-wasp.system.schedule()
+from gadgetbridge import *
+wasp.Wasp.start()
