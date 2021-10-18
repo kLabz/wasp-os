cb8cbf472e4eb11420009e02523fb446a201da7a
k <k@klabz.org>
2021-10-18 23:11:20 +0200

    Tweak main.py

--- wasp/main.py
+++ wasp/main.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 # Copyright (C) 2020 Daniel Thompson
 
-from wasp import Wasp as wasp
-# from gadgetbridge import *
-wasp.start()
+import wasp
+from gadgetbridge import *
+wasp.Wasp.start()
